5c3ea23645e6127b0d698a3c5ff680d6f6900e74
Marc Schwarze <marc.schwarze@posteo.de>
2020-04-10 15:44:35 +0200

    Add new folder first/explain/comments. Inside the main.go a demonstration about 'comments'.

---
+++ first/explain/comments/main.go
@@ -0,0 +1,14 @@
+// Package main makes this package an executable program.
+package main
+
+import "fmt"
+
+/*
+main function
+Go executes this program using this function.
+there should be only one main file in a main package.
+Executable programs are also called "commands".
+*/
+func main() {
+	fmt.Println("Hello Gopher!")
+}
